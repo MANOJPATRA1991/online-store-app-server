d281ab52c8d4e44ba5846fec1ed269efdce8f977
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-26 12:37:32 +0530

    Update app configurations

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var logger = require('morgan');
 
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
-var expressHandlebars = require('express-handlebars');
-var expressValidator = require('express-validator');
+// var expressHandlebars = require('express-handlebars');
+// var expressValidator = require('express-validator');
 var session = require('express-session');
 
-var flash = require('connect-flash');
+// var flash = require('connect-flash');
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -19,26 +20,42 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+var cors = require('cors');
+
+//requires the model with Passport-Local Mongoose plugged in
+var User = require('./models/User');
+
+var config = require('./config');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
-var config = require('./config');
+var uploads = require('./routes/upload');
+var items = require('./routes/items');
 
 mongoose.connect(config.mongoUrl);
 
 var db = mongoose.connection;
 
+//use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+//use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', expressHandlebars({defaultLayout: 'layout'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json({limit:'50mb'})); 
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+
 app.use(cookieParser());
 
 // Set static folder
@@ -56,41 +73,46 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Express validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.'),
-        root = namespace.shift(),
-        formParam = root;
+// app.use(expressValidator({
+//   errorFormatter: function(param, msg, value) {
+//     var namespace = param.split('.'),
+//         root = namespace.shift(),
+//         formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param: formParam,
-      msg: msg,
-      value: value
-    }
-  }
-}));
+//     while(namespace.length) {
+//       formParam += '[' + namespace.shift() + ']';
+//     }
+//     return {
+//       param: formParam,
+//       msg: msg,
+//       value: value
+//     }
+//   }
+// }));
 
 // Connect flash
-app.use(flash());
+// app.use(flash());
 
 // Global vars
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-});
+// app.use(function(req, res, next) {
+//   res.locals.success_msg = req.flash('success_msg');
+//   res.locals.error_msg = req.flash('error_msg');
+//   res.locals.error = req.flash('error');
+// });
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/upload', uploads);
+app.use('/items', items);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
