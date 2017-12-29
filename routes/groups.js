35b105b17b7a9adb4097074709f64f2e56d0a805
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-29 11:05:05 +0530

    Update group routing

--- routes/groups.js
+++ routes/groups.js
@@ -7,10 +7,8 @@ var Verify = require('./verify');
 
 var bodyParser = require('body-parser');
 
-
-router.route('/newGroup')
 // Add new group
-.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, (req, res, next) => {
+router.post('/newGroup', Verify.verifyOrdinaryUser, Verify.verifyAdmin, (req, res, next) => {
     Group.find((err, groups) => {
         if(err) {
             console.log(err);
@@ -23,38 +21,36 @@ router.route('/newGroup')
                 console.log(nwGrp);
             })
         }
-        if (!groups[0].group.includes(req.body.newGroup)) {
-            Group.findOneAndUpdate(
-                { _id: groups[0]._id }, 
-                {
-                    $push: {
-                        group: req.body.newGroup
-                    }
-                }, 
-                (err, grp) => {
-                    if (err) {
-                        next(err);
-                    }
-                    console.log(grp);
-                    res.status(200).json({
-                        msg: `${req.body.newGroup} Group Added`
-                    }
-                );
-            });
-        } else {
-            res.status(409).json({
-                msg: `Group ${req.body.newGroup} already exists.`
-            });
-        }
+        Group.findOneAndUpdate(
+            { _id: groups[0]._id }, 
+            {
+                $addToSet: {
+                    group: req.body.newGroup
+                }
+            },
+            {new: true}, 
+            (err, grp) => {
+                if (err) {
+                    next(err);
+                }
+                console.log(grp);
+                res.status(200).json({
+                    msg: `${req.body.newGroup} Group Added`,
+                    groups: grp.group
+                }
+            );
+        });
     });
 })
 
+// Get all groups
 router.route('/')
 // Get all groups
 .get((Req, res, next) => {
     Group.find((err, groups) => {
         if(err) {
             next(err);
+            console.log(err);
         }
         res.status(200).json(groups[0].group);
     });
