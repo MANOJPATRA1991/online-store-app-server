0ca7600e6aa5eb0973d0676e5c99ce86ddf2c4ed
MANOJ PATRA <patra.manoj0@gmail.com>
2017-12-29 11:05:52 +0530

    Update uploads routing to use multer

--- routes/upload.js
+++ routes/upload.js
@@ -1,32 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
 // Require multer for the file uploads
 var multer = require('multer');
 
-// Set the directory for the uploads to the uploaded to
-var DIR = '../public/uploads/';
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: '../osaserver/public/uploads',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 
-/**
- * Define the type of upload multer 
- * would be doing and pass in its destination
- */
-var upload = multer({dest: DIR}).single('photo');
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('photo');
 
-// Our file upload function.
-router.post('/', function (req, res, next) {
-     var path = '';
-     upload(req, res, function (err) {
-        if (err) {
-          // An error occurred when uploading
-          console.log(err);
-          return res.status(422).send("An error occured.")
-        }  
-        
-        // No error occured.
-        path = req.file.path;
-        return res.send("Upload Completed for "+path); 
-  });     
-})
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+// Upload image
+router.post('/', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.status(200).json({
+        msg: 'File Uploaded!',
+        file: `uploads/${req.file.filename}`
+      });
+    }
+  });
+});
 
 module.exports = router;
